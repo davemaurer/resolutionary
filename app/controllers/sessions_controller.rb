f7c8b757b18e3193f8401ea46bd1fd9bf37f3c9d
David Maurer <davemaurermail@gmail.com>
2015-09-22 08:11:49 -0600

    added sessions and dashboard controller, routes, current_user method, twitter_client and user model omniauth

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+  def create
+    if user = User.from_omniauth(oauth_data)
+      session[:user_id] = user.id
+      redirect_to dashboard_path
+    else
+      redirect_to root_path
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
+  private
+
+  def oauth_data
+    request.env['omniauth.auth']
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,23 @@
 class User < ActiveRecord::Base
+  has_many :resolutions
+
+  def self.from_omniauth(auth_info)
+    user = find_or_create_by(uid: auth_info.uid)
+    user.update_attributes(
+      name: auth_info.extra.raw_info.name,
+      profile_picture: auth_info.extra.raw_info.profile_image_url,
+      oauth_token: auth_info.credentials.token,
+      oauth_token_secret: auth_info.credentials.secret
+    )
+    user
+  end
+
+  def twitter_client
+    @client ||= Twitter::REST::Client.new do |config|
+      config.consumer_key = ENV['TWITTER_KEY']
+      config.consumer_secret = ENV['TWITTER_SECRET']
+      config.access_token = oauth_token
+      config.access_token_secret = oauth_token_secret
+    end
+  end
 end
