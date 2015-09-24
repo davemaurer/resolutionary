5b81220118ec97c9a2de003eb2940894124efe90
David Maurer <davemaurermail@gmail.com>
2015-09-24 11:31:23 -0600

    added twitter feed and crud functionality for resolutions

--- app/controllers/resolutions_controller.rb
+++ app/controllers/resolutions_controller.rb
@@ -10,17 +10,38 @@ class ResolutionsController < ApplicationController
   end
 
   def create
-    @resolution = Resolution.where(resolution_params).first_or_create
-    flash[:notice] = "Resolution created!"
-    redirect_to dashboard_path
+    if current_user
+      current_user.resolutions.where(resolution_params).first_or_create
+      flash[:notice] = "Resolution created!"
+      redirect_to dashboard_path
+    end
   end
 
   def edit
+    @resolution = Resolution.find(params[:id])
+  end
+
+  def update
+    resolution = Resolution.find(params[:id])
+    if resolution.update(resolution_params)
+      flash[:notice] = "Resolution Updated Successfully"
+      redirect_to dashboard_path
+    else
+      flash[:notice] = "Please try again"
+      render :edit
+    end
+  end
+
+  def destroy
+    resolution = Resolution.find(params[:id])
+    resolution.destroy
+    flash[:notice] = "Resolution is gone forever!"
+    redirect_to dashboard_path
   end
 
   private
 
   def resolution_params
-    params.require(:resolution).permit(:title, :description, :motive_image)
+    params.require(:resolution).permit(:user_id, :title, :description, :motive_image)
   end
 end
