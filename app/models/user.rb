d8618d4efdbc96c89f48ea481f923897ba444d89
David Maurer <davemaurermail@gmail.com>
2015-09-24 13:15:19 -0600

    added profile page for users and cleaned up dashboard. added twitter feed with motivations tweets

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ActiveRecord::Base
 
   def self.from_omniauth(auth_info)
     user = find_or_create_by(uid: auth_info.uid)
-    user.update_attributes(
-      name:               auth_info.extra.raw_info.name,
-      profile_image:      auth_info.extra.raw_info.profile_image_url,
-      oath_token:        auth_info.credentials.token,
-      oauth_token_secret: auth_info.credentials.secret
-    )
+    user.update_attributes(name:               auth_info.extra.raw_info.name,
+                           profile_image:      auth_info.extra.raw_info.profile_image_url,
+                           oath_token:         auth_info.credentials.token,
+                           oauth_token_secret: auth_info.credentials.secret
+                           )
+
     user
   end
 
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
     @client ||= Twitter::REST::Client.new do |config|
       config.consumer_key = ENV['TWITTER_KEY']
       config.consumer_secret = ENV['TWITTER_SECRET']
-      config.access_token = oauth_token
+      config.access_token = oath_token
       config.access_token_secret = oauth_token_secret
     end
   end
@@ -27,5 +27,9 @@ class User < ActiveRecord::Base
 
   def background_picture
     twitter_client.user.profile_background_image_url_https
+  end
+
+  def inspirational_tweets
+    twitter_client.search("#motivation -rt").first(10)
   end
 end
