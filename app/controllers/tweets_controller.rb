b4d5a09a21dbe372bf87505fcc6ef934fbd3e9ab
David Maurer <davemaurermail@gmail.com>
2015-09-26 17:15:28 -0600

    Tweet resolutions
    This completes [waffle card 10](https://waffle.io/davemaurer/resolutionary/cards/56045f118a19da3700d45a89)
    
    We have decided to start actually writing tests, so this was the first feature
    on the app that was test driven. The helper methods in the tests are a bit
    dense, as we needed to jump through some hoops to mock out Twitter, and we
    don't yet have enough context to figure out a better way to extract this out
    and have it be a bit less noisy. At the very least, the test itself
    demonstrates intent pretty well.
    
    The feature was fairly straightforward to implement, and I don't think there
    was much that stood out as weird. We learned about keyword arguments, which we
    hadn't seen before, and learned how to use. We also stumbled a little bit when
    we realized that even when we're making an expectation on a request having been
    made, we still need to stub it before the request is actually made, since
    WebMock doesn't know to allow it.

---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,17 @@
+class TweetsController < ApplicationController
+  def create
+    tweet_body = "My new resolution is: #{resolution.title}!"
+    send_tweet(body: tweet_body)
+    redirect_to dashboard_path, flash: { notice: "Tweeted successfully!" }
+  end
+
+  private
+
+  def resolution
+    @_resolution ||= Resolution.find(params[:resolution_id])
+  end
+
+  def send_tweet(body:)
+    current_user.twitter_client.update(body)
+  end
+end
