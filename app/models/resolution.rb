d338efaebd97560a581ea87347e1497afde7c57d
David Maurer <davemaurermail@gmail.com>
2015-09-27 12:55:51 -0600

    changed max resolution title size to 115 so twitter will let it post

--- app/models/resolution.rb
+++ app/models/resolution.rb
@@ -1,5 +1,5 @@
 class Resolution < ActiveRecord::Base
-  validates :title, presence: true, length: { maximum: 140 }
+  validates :title, presence: true, length: { maximum: 115 }
 
   has_many :goals
   belongs_to :user
