bd66e1a8c744a58ec3cb95d3adcefdf53430652d
David Maurer <davemaurermail@gmail.com>
2015-09-29 12:18:24 -0600

    changed view to show current goals on top and completed goals
    below current goals. No buttons or edit option for completed goals

--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,3 +1,11 @@
 class Goal < ActiveRecord::Base
   belongs_to :resolution
+
+  def self.completed_goals
+    where(completed: true)
+  end
+
+  def self.current_goals
+    where(completed: false)
+  end
 end
