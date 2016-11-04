d1b50731839b438a8d9ef1af97c8187f37ae75f0
David Maurer <davemaurermail@gmail.com>
2016-11-04 16:36:44 -0600

    Add tests for users being reminded of resolutions they have neglected.

--- app/models/resolution.rb
+++ app/models/resolution.rb
@@ -5,12 +5,27 @@ class Resolution < ActiveRecord::Base
   has_many :reminders, dependent: :destroy
   belongs_to :user
 
+  FORGOTTEN_THRESHOLD = 30.days.ago
   def self.completed
     where(finished: true)
   end
 
   def self.current
     where(finished: false)
+  end
+
+  def self.forgotten
+    empty_old_resolutions | resolutions_with_only_inactive_goals
+  end
+
+  def self.empty_old_resolutions
+    where("updated_at < '#{FORGOTTEN_THRESHOLD}'")
+  end
+
+  def self.resolutions_with_only_inactive_goals
+    Resolution
+      .joins(:goals)
+      .where("goals.updated_at < '#{FORGOTTEN_THRESHOLD}'")
   end
 
   def completed_goals
---
+++ spec/models/resolution_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+describe Resolution, ".forgotten" do
+  let(:user) { User.create(name: 'Bob') }
+  let!(:resolution) { Resolution.create(title: 'Live Forever',
+                                       description: 'So I can see the end of the universe',
+                                       user_id: user.id) }
+
+  context "resolutions that have no goals" do
+    it "returns those resolutions that are older than 30 days" do
+      resolution.update_attributes(updated_at: 31.days.ago)
+      expect(Resolution.forgotten).to eq([resolution])
+    end
+    it "does not return resolutions younger than 30 days" do
+      expect(Resolution.forgotten).to be_empty
+    end
+  end
+
+  context "resolutions that have goals" do
+    it "returns those resolutions that have no goals completed within 30 days" do
+      oldie = Goal.create(name: "old", resolution_id: resolution.id)
+      oldie.update(completed: true)
+      oldie.update_attributes created_at: 31.days.ago, updated_at: 31.days.ago
+      expect(Resolution.forgotten).to eq([resolution])
+    end
+
+    it "does not return resolutions that have any goals completed within 30 days" do
+      oldie = Goal.create(name: "old", resolution_id: resolution.id)
+      oldie.update_attributes created_at: 31.days.ago
+      oldie.update(completed: true)
+      expect(Resolution.forgotten).to be_empty
+    end
+
+    it "does not return resolutions that have any goals added within 30 days" do
+      Goal.create(name: "goal", resolution_id: resolution.id)
+      expect(Resolution.forgotten).to be_empty
+    end
+  end
+end
